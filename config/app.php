d6da4f7ef40655dae209169c73ea41018c2e4185
Travis Flatt <tflatt74@gmail.com>
2016-01-19 21:05:41 -0600

    Service Provider

--- config/app.php
+++ config/app.php
@@ -147,7 +147,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\View\GeneratorsServiceProvider::class,
 
         /*
          * Application Service Providers...
